b617cecbbd8f868529b74113692e64fc3073de6f
mj65536 <119717335+Mj65536@users.noreply.github.com>
2023-07-29 20:51:45 +0800

    test

--- src/ArraySearch.java
+++ src/ArraySearch.java
@@ -2,47 +2,53 @@ public class ArraySearch
 {
     public static void main(String[] args)
     {
-        int[] arr01 = new int[]{1,3,5,0,9,55,2};
-        int target01 = 0;
-        boolean flag = true;
-        for(int i = 0;i < arr01.length;i++)
-        {
-            if(arr01[i] == target01)
-            {
-                System.out.print("Found target!");
-                flag = false;
-                break;
-            }
-        }
-        if(flag)
-        {
-            System.out.print("Not found!");
-        }
-        //线性查找方法
+//        int[] arr01 = new int[]{1,3,5,0,9,55,2};
+//        int target01 = 0;
+//        boolean flag01 = true;
+//        for(int i = 0;i < arr01.length;i++)
+//        {
+//            if(arr01[i] == target01)
+//            {
+//                System.out.print("Found target01!");
+//                flag01 = false;
+//                break;
+//            }
+//        }
+//        if(flag01)
+//        {
+//            System.out.print("Not Found!");
+//        }
+        //线性查找方法,时间复杂度为o(N)
 
 
         int[] arr02 = new int[]{-100,-40,-32,-16,0,19,48,64,99};
         int target02 = 64;
         int head = 0;
         int end = arr02.length - 1;
-        int mid = arr02[arr02.length/2];
-        if(head < end)
+        boolean flag02 = true;
+        while(head < end)
         {
-            if(target02 < arr02[mid])
+            int middle = (head + end) / 2;
+            if(target02 == arr02[middle])
             {
-
-
+                System.out.println("Found target02!");
+                flag02 = false;
+                break;
             }
-
-
+            else if(target02 > arr02[middle])
+            {
+                head = middle + 1;
+            }
+            else
+            {
+                end = middle - 1;
+            }
         }
 
-
-
-
-
-
-
-
+        if(flag02)
+        {
+            System.out.println("Not Found!");
+        }
+        //二分法查找,时间复杂度为o(log N)但是需要数组提前排序
     }
 }
