f96dc16545898623279c1d1112f859a87a6788ae
mj65536 <119717335+Mj65536@users.noreply.github.com>
2023-09-08 23:40:01 +0800

    09_08

---
+++ src/SeasonTest_0908.java
@@ -0,0 +1,49 @@
+//import java.security.PrivateKey;
+//
+//public class SeasonTest_0908
+//{
+//    public static void main(String[] args)
+//    {
+//        System.out.println(Season.SPRING);
+//
+//    }
+//}
+//
+//
+//class Season
+//{
+//    private final String seasonName;
+//    private final String seasonDesc;
+//    private Season(String seasonName,String seasonDesc)
+//    {
+//        this.seasonName = seasonName;
+//        this.seasonDesc = seasonDesc;
+//
+//    }
+//    //私有化类的构造器
+//
+//
+//    public String getSeasonName()
+//    {
+//        return seasonName;
+//    }
+//
+//    public String getSeasonDesc()
+//    {
+//        return seasonDesc;
+//    }
+//
+//    public static fianl Season SPRING = new Season("春天","春天就是春天");
+//    public static fianl Season SUMMER = new Season("夏天","夏天就是夏天");
+//    public static fianl Season AUTUMN = new Season("秋天","秋天就是秋天");
+//    public static fianl Season WINTER = new Season("冬天","冬天就是冬天");
+//
+//    @Override
+//    public String toString() {
+//        return "Season{" +
+//                "seasonName='" + seasonName + '\'' +
+//                ", seasonDesc='" + seasonDesc + '\'' +
+//                '}';
+//    }
+//}
+
